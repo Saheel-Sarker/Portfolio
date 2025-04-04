c4429fcc370966a3f6632943c0b0ab03030d4f32
saheel <saheel.sarker@gmail.com>
2025-04-04 13:12:12 -0600

    changed github location

--- app/footer.tsx
+++ app/footer.tsx
@@ -70,7 +70,7 @@ export function Footer() {
   return (
     <footer className="mt-24 border-t border-zinc-100 px-0 py-4 dark:border-zinc-800">
       <div className="flex items-center justify-between">
-        <a href="https://github.com/ibelick/nim" target="_blank">
+        <a href="https://github.com/saheel-sarker" target="_blank">
           <span  className="text-sm text-zinc-500">© 2024 Saheel Sarker</span>
         </a>
         <div className="text-sm text-zinc-400">
